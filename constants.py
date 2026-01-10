261b0fda62b1fc9eff6c97b9c0876211c7830e4f
vivo-777 <chavdavivek559@gmail.com>
2026-01-11 01:06:28 +0530

    Fixed the login bug

---
+++ constants.py
@@ -0,0 +1,36 @@
+
+
+teams_per_year_static = {
+    2008: ['Chennai Super Kings', 'Delhi Daredevils', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Deccan Chargers'],
+    2009: ['Chennai Super Kings', 'Delhi Daredevils', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Deccan Chargers'],
+    2010: ['Chennai Super Kings', 'Delhi Daredevils', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Deccan Chargers'],
+    2011: ['Chennai Super Kings', 'Delhi Daredevils', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Deccan Chargers', 'Pune Warriors India', 'Kochi Tuskers Kerala'],
+    2012: ['Chennai Super Kings', 'Delhi Daredevils', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Deccan Chargers', 'Pune Warriors India'],
+    2013: ['Chennai Super Kings', 'Delhi Daredevils', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Sunrisers Hyderabad', 'Pune Warriors India'],
+    2014: ['Chennai Super Kings', 'Delhi Daredevils', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Sunrisers Hyderabad'],
+    2015: ['Chennai Super Kings', 'Delhi Daredevils', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Sunrisers Hyderabad'],
+    2016: ['Delhi Daredevils', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Mumbai Indians', 'Royal Challengers Bangalore', 'Sunrisers Hyderabad', 'Rising Pune Supergiant', 'Gujarat Lions'],
+    2017: ['Delhi Daredevils', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Mumbai Indians', 'Royal Challengers Bangalore', 'Sunrisers Hyderabad', 'Rising Pune Supergiant', 'Gujarat Lions'],
+    2018: ['Chennai Super Kings', 'Delhi Daredevils', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Sunrisers Hyderabad'],
+    2019: ['Chennai Super Kings', 'Delhi Capitals', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Sunrisers Hyderabad'],
+    2020: ['Chennai Super Kings', 'Delhi Capitals', 'Kings XI Punjab', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Sunrisers Hyderabad'],
+    2021: ['Chennai Super Kings', 'Delhi Capitals', 'Punjab Kings', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Sunrisers Hyderabad'],
+    2022: ['Chennai Super Kings', 'Delhi Capitals', 'Punjab Kings', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Sunrisers Hyderabad', 'Gujarat Titans', 'Lucknow Super Giants'],
+    2023: ['Chennai Super Kings', 'Delhi Capitals', 'Punjab Kings', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bangalore', 'Sunrisers Hyderabad', 'Gujarat Titans', 'Lucknow Super Giants'],
+    2024: ['Chennai Super Kings', 'Delhi Capitals', 'Punjab Kings', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bengaluru', 'Sunrisers Hyderabad', 'Gujarat Titans', 'Lucknow Super Giants'],
+    2025: ['Chennai Super Kings', 'Delhi Capitals', 'Punjab Kings', 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', 'Royal Challengers Bengaluru', 'Sunrisers Hyderabad', 'Gujarat Titans', 'Lucknow Super Giants']
+}
+
+model_translation = {
+    'Delhi Daredevils': 'Delhi Capitals',
+    'Kings XI Punjab': 'Punjab Kings',
+    'Deccan Chargers': 'Sunrisers Hyderabad',
+    'Royal Challengers Bangalore': 'Royal Challengers Bengaluru',
+    'Rising Pune Supergiants': 'Rising Pune Supergiant',
+}
+
+cities = [
+    'Ahmedabad', 'Bangalore', 'Chennai', 'Delhi', 'Dharamsala', 
+    'Hyderabad', 'Jaipur', 'Kolkata', 'Lucknow', 'Mohali', 
+    'Mumbai', 'Pune', 'Visakhapatnam', 'Abu Dhabi', 'Sharjah', 'Dubai'
+]
